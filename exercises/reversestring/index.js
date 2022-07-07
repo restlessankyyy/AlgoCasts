6e6d019a017731406db51630c72dd130ef31d207
Ankit Raj <rajankit749@gmail.com>
2022-07-07 23:26:15 +0000

    add rev sol

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,59 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// 2nd solution
+function reverse(str) {
+    let reversed = '';
+    for ( let char of str){
+        reversed = char + reversed;
+        
+        console.log(reversed);     
+        debugger;
 
+    }
+    return reversed;
+}
+
+reverse('hello');
 module.exports = reverse;
+
+
+
+// 1st solution
+// function reverse(str) {
+//     const splitString = str.split('');
+//     const reversedString = splitString.reverse();
+//     const reversed = reversedString.join('');
+//     return reversed;
+// }
+
+
+
+// 2nd solution
+// function reverse(str) {
+//     let reversed = '';
+//     for ( let char of str){
+//         reversed = char + reversed;
+//         console.log(reversed);       
+// // h
+// // eh
+// // leh
+// // lleh
+// // olleh
+// // olleh
+//     }
+//     console.log(reversed);
+//     return reversed;
+    
+// }
+
+
+
+// 3rd Solution
+// function reverse(str) {
+//     const arr = str.split('');
+//     console.log(arr);
+//     const reverseString = arr.reduce((rev, char) => char + rev, '');
+//     console.log(reverseString);
+//     return reverseString;  
+// }
